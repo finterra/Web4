6379a7de15dc7dbfc8eeabd8be978fe010217cfa
nagarajmanjunath <38091008+nagarajmanjunath@users.noreply.github.com>
2018-06-14 11:29:44 +0800

    Update blockchain.js

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,113 +1,116 @@
-var server = require('../src/server');
-var errormsg;
-var parameters = [];
+  var server = require('../src/server');
+  var errormsg;
+  var parameters = [];
 
-var server= new server();
-module.exports = class BlockChain
-{  
-   constructor(server)
-     {
-         this.server=server;
-         
-     }
+  var server = new server();
+  module.exports = class BlockChain {  
+    constructor(server)
+      {
+          this.server=server;
+          
+      }
 
-/**
- * Get blockchain info.
- *
- * @param {module:rpc/rpc~methodCallback} callback - The callback function.
- */
-getInfo = function (cb) {
+    /**
+     * Get blockchain info.
+     *
+     * @param {module:rpc/rpc~methodCallback} callback - The callback function.
+     */
 
-  parameter={}
-  server.serverPost("burrow.getBlockchainInfo",this.server,parameter,function(error,data){
-    if(error) return cb(error);
-    return cb(null, data);
-  });
-}
+    getInfo(cb) {
+      parameter = {}
+      server.serverPost("burrow.getBlockchainInfo",this.server,parameter,function(error,data){
+        if(error) return cb(error);
+        return cb(null, data);
+      });
+    }
 
-/**
- * Get the chain id.
- *
- * @param {module:rpc/rpc~methodCallback} callback - The callback function.
- */
-getChainId = function (cb) {
+    /**
+     * Get the chain id.
+     *
+     * @param {module:rpc/rpc~methodCallback} callback - The callback function.
+     */
 
-  parameters={};
-  server.serverPost("burrow.getChainId",this.server,parameters,function(error,data){
-    if(error) return cb(error);
-    return cb(null, data);
-  });
-}
+    getChainId(cb) {
+      parameters = {};
+      server.serverPost("burrow.getChainId",this.server,parameters,function(error,data){
+        if(error) return cb(error);
+        return cb(null, data);
+      });
+    }
 
-/**
- * Get the genesis hash.
- *
- * @param {module:rpc/rpc~methodCallback} callback - The callback function.
- */
-getGenesisHash = function (cb) {
-  parameters={};
-  server.serverPost("burrow.getGenesisHash",this.server,parameters,function(error,data){
-    if(error) return cb(error);
-    return cb(null, data);
-  });
-}
+    /**
+     * Get the genesis hash.
+     *
+     * @param {module:rpc/rpc~methodCallback} callback - The callback function.
+     */
 
-/**
- * Get the latest block height.
- *
- * @param {module:rpc/rpc~methodCallback} callback - The callback function.
- */
-getLatestBlockHeight = function (cb) {
-  parameters={};
-  server.serverPost("burrow.getLatestBlockHeight",this.server,parameters,function(error,data){
-    if(error) return cb(error);
-    return cb(null, data);
-  });
-}
+    getGenesisHash(cb) {
+      parameters = {};
+      server.serverPost("burrow.getGenesisHash",this.server,parameters,function(error,data){
+        if(error) return cb(error);
+        return cb(null, data);
+      });
+    }
 
-/**
- * Get the latest block.
- *
- * @param {module:rpc/rpc~methodCallback} callback - The callback function.
- */
-getLatestBlock = function (cb) {
+    /**
+     * Get the latest block height.
+     *
+     * @param {module:rpc/rpc~methodCallback} callback - The callback function.
+     */
 
-  parameters={};
-  server.serverPost("burrow.getLatestBlock",this.server,parameters,function(error,data){
-    if(error) return cb(error);
-    return cb(null, data);
-  });
- 
-}
+    getLatestBlockHeight(cb) {
+      parameters = {};
+      server.serverPost("burrow.getLatestBlockHeight",this.server,parameters,function(error,data){
+        if(error) return cb(error);
+        return cb(null, data);
+      });
+    }
 
-/**
- * Get the blocks from 'minHeight' to 'maxHeight'.
- *
- * TODO out of bounds checks?
- *
- * @param {module:util~FieldFilter|module:util~FieldFilter[]} [filter] - Filter the search.
- * @param {module:rpc/rpc~methodCallback} callback - The callback function.
- */
-getBlocks = function (cb) {
-  parameters = {"filters":[]}
-  server.serverPost("burrow.getBlocks",this.server,parameters,function(error,data){
-    if(error) return cb(error);
-    return cb(null, data);
-  });
-}
+    /**
+     * Get the latest block.
+     *
+     * @param {module:rpc/rpc~methodCallback} callback - The callback function.
+     */
 
-/**
- * Get the block with the given block-number, or 'height'.
- *
- * @param {number} height - The block height.
- * @param {module:rpc/rpc~methodCallback} callback - The callback function.
- */
-getBlock = function (height, cb) { 
-  
-  parameters = {"height":height}
-  server.serverPost("burrow.getBlock",this.server,parameters,function(error,data){
-    if(error) return cb(error);
-    return cb(null, data);
-  });
-}
-}
+    getLatestBlock(cb) {
+      parameters = {};
+      server.serverPost("burrow.getLatestBlock",this.server,parameters,function(error,data){
+        if(error) return cb(error);
+        return cb(null, data);
+      });
+
+    }
+
+    /**
+     * Get the blocks from 'minHeight' to 'maxHeight'.
+     *
+     * TODO out of bounds checks?
+     *
+     * @param {module:util~FieldFilter|module:util~FieldFilter[]} [filter] - Filter the search.
+     * @param {module:rpc/rpc~methodCallback} callback - The callback function.
+     */
+
+    getBlocks(cb) {
+      parameters = {"filters":[]}
+      server.serverPost("burrow.getBlocks",this.server,parameters,function(error,data){
+        if(error) return cb(error);
+        return cb(null, data);
+      });
+    }
+
+    /**
+     * Get the block with the given block-number, or 'height'.
+     *
+     * @param {number} height - The block height.
+     * @param {module:rpc/rpc~methodCallback} callback - The callback function.
+     */
+
+    getBlock(height, cb) { 
+      
+      parameters = {"height":height}
+      server.serverPost("burrow.getBlock",this.server,parameters,function(error,data){
+        if(error) return cb(error);
+        return cb(null, data);
+      });
+    }
+  }
