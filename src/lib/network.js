e7a5d3e1dd97e56cf5579bdb64dc18389559c3b0
nagarajmanjunath <38091008+nagarajmanjunath@users.noreply.github.com>
2018-06-25 19:37:39 +0800

    Update network.js

--- src/lib/network.js
+++ src/lib/network.js
@@ -4,10 +4,12 @@
  * @author Nagaraj Manjunath
  * @module Network
  */
-
+var server= require('./server');
+var server = new server();
+let errormsg;
 module.exports = class Network{
-constructor (server) {
-this.server=server;
+constructor (serverUrl) {
+this.server=serverUrl;
 }
 /**
  * Get the network info.
@@ -15,11 +17,19 @@ this.server=server;
  * @param {module:rpc/rpc~methodCallback} callback - The callback function.
  */
 getInfo (cb) {
+  if(!this.server){
+    errormsg= "Method:getNetworkInfo ; error:server url  is undefined";
+    return errormsg;
+  } 
+  else
+  {
     let parameters= {}
     server.serverPost("burrow.getNetworkInfo",this.server,parameters,function(error,data){
       if(error)  cb(error);
        cb(null, data);
     });
+  }
+  
     
   }
   
@@ -29,6 +39,10 @@ getInfo (cb) {
    * @param {module:rpc/rpc~methodCallback} callback - The callback function.
    */
 getClientVersion (cb) {
+  if(!this.server){
+    errormsg= "Method:getClientVersion; error:server url  is undefined";
+    return errormsg;
+  } 
     let parameters= {}
     server.serverPost("burrow.getClientVersion",this.server,parameters,function(error,data){
       if(error)  cb(error);
@@ -42,6 +56,10 @@ getClientVersion (cb) {
    * @param {module:rpc/rpc~methodCallback} callback - The callback function.
    */
 getMoniker (cb) {
+  if(!this.server){
+    errormsg= "Method:getMoniker; error:server url  is undefined";
+    return errormsg;
+  } 
     let parameters= {}
     server.serverPost("burrow.getMoniker",this.server,parameters,function(error,data){
       if(error)  cb(error);
@@ -56,11 +74,18 @@ getMoniker (cb) {
    * @param {module:rpc/rpc~methodCallback} callback - The callback function.
    */
 isListening  (cb) {
+  if(!this.server){
+    errormsg= "Method:isListening; error:server url  is undefined";
+    return errormsg;
+  } 
+  else{
     let parameters= {}
     server.serverPost("burrow.isListening",this.server,parameters,function(error,data){
       if(error)  cb(error);
        cb(null, data);
     });
+  }
+   
    
   }
   
@@ -70,11 +95,18 @@ isListening  (cb) {
    * @param {module:rpc/rpc~methodCallback} callback - The callback function.
    */
 getListeners (cb) {
+  if(!this.server){
+    errormsg= "Method:getListeners; error:server url  is undefined";
+    return errormsg;
+  } 
+  else{
     let parameters= {}
     server.serverPost("burrow.getListeners",this.server,parameters,function(error,data){
       if(error)  cb(error);
        cb(null, data);
     });
+  }
+    
 
   }
   
@@ -84,12 +116,20 @@ getListeners (cb) {
    * @param {module:rpc/rpc~methodCallback} callback - The callback function.
    */
 getPeers(cb) {
+  if(!this.server){
+    errormsg= "Method:getPeers; error:server url  is undefined";
+    return errormsg;
+  } 
+  else
+  {
     let parameters= {}
     server.serverPost("burrow.getPeers",this.server,parameters,function(error,data){
       if(error)  cb(error);
        cb(null, data);
     });
     this.server.getPeers(callback)
+  }
+   
   }
   
   /**
@@ -98,12 +138,19 @@ getPeers(cb) {
    * @param {module:rpc/rpc~methodCallback} callback - The callback function.
    */
 getPeer(address, cb) {
-
+  if(!this.server || !address){
+    errormsg= "Method:getPeer; error: invalid argument";
+    return errormsg;
+  } 
+  else
+  {
     let parameters= {"address":address}
     server.serverPost("burrow.getPeer",this.server,parameters,function(error,data){
       if(error) cb(error);
         cb(null, data);
     });
+  }
+ 
   }
   
 }
