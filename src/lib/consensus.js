0871c810d52e286d962252e627fba0d0e5c21955
nagarajmanjunath <38091008+nagarajmanjunath@users.noreply.github.com>
2018-06-25 19:39:24 +0800

    Update consensus.js

--- src/lib/consensus.js
+++ src/lib/consensus.js
@@ -1,50 +1,61 @@
 
-  /**
-   * @file consensus.js
-   * @fileOverview Factory module for the Events class.
-   * @author Nagaraj Manjunath
-   * @module Consensus
-   */
-  module.exports = class Consensus
-  {
-  /**
-   * Get a list of all validators.
-   *
-   * @param {module:rpc/rpc~methodCallback} callback - The callback function.
-   */
-  getValidators(cb) {
-    if(!this.server){        
+      /**
+       * @file consensus.js
+       * @fileOverview Factory module for the Events class.
+       * @author Nagaraj Manjunath
+       * @module Consensus
+       */
+
+      var server          = require('./server');
+      var errormsg;
+      var parameters      = [];
+      var server          = new server();
+
+      module.exports = class Consensus
+      {
+        constructor(serverUrl)
+        {
+          this.server = serverUrl;
+          
+        }
+      /**
+       * Get a list of all validators.
+       *
+       * @param {module:rpc/rpc~methodCallback} callback - The callback function.
+       */
+      getValidators(cb) {
+      if(!this.server){        
       errormsg = "method:getValidators error: server url  is not defined."
-       cb(null,errormsg);
-  }
-  else{
-    let parameters = {}
-    server.serverPost("burrow.getValidators",this.server,parameters,function(error,data){
+      cb(null,errormsg);
+      }
+      else{
+      let parameters = {}
+      server.serverPost("burrow.getValidators",this.server,parameters,function(error,data){
       if(error)  cb(error);
-       cb(null, data);
-    });
+      cb(null, data);
+      });
 
-  }
-    
-    }
-  
-    /**
-     * Get the consensus state.
-     *
-     * @param {module:rpc/rpc~methodCallback} callback - The callback function.
-     */
-  getState(cb) {
-    if(!this.server){        
+      }
+
+      }
+
+      /**
+       * Get the consensus state.
+       *
+       * @param {module:rpc/rpc~methodCallback} callback - The callback function.
+       */
+      getState(cb) {
+      if(!this.server){        
       errormsg = "method:getState error: server url  is not defined."
-       cb(null,errormsg);
-  }else{
-    let parameters = {}
-    server.serverPost("burrow.getConsensusState",this.server,parameters,function(error,data){
+      cb(null,errormsg);
+      }else{
+      let parameters = {}
+      server.serverPost("burrow.getConsensusState",this.server,parameters,function(error,data){
       if(error)  cb(error);
-       cb(null, data);
-    });
-  }
-  }
-    
+      cb(null, data);
+      });
+      }
+      }
 
-  }
+
+      }
