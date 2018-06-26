ed5ea8b392863effbfbe58b6a4365e7cfaa5c365
nagarajmanjunath <38091008+nagarajmanjunath@users.noreply.github.com>
2018-06-26 18:31:56 +0800

    changes made in transcation .js

--- src/lib/transaction.js
+++ src/lib/transaction.js
@@ -1,13 +1,21 @@
 
-      var server = require('./server');
+      
+      var requestserver = require('./server');
+      let server = new requestserver();
       var errormsg;
-      var parameters = [];
-      var server = new server();
+    
+      /**
+     * @file Transaction.js
+     * @fileOverview Factory module for the Transaction class.
+     * @author Nagaraj Manjunath
+     * @module Transaction
+     */
 
       module.exports = class Transaction {
 
       constructor(url)
       {
+            
       this.server = url;
 
       };
@@ -21,7 +29,7 @@
       }
       else
       {
-      parameters = {}
+     let parameters = {}
       server.serverPost("burrow.getUnconfirmedTxs",this.server,parameters,function(error,data){
       if(error)  cb(error,null);
       cb(null, data);
@@ -44,7 +52,7 @@
       }
       else
       {
-      parameters =    {"priv_key"   : private_key,
+     let parameters =    {"priv_key"   : private_key,
       "to_address" : to_address,
       "amount"     : amount}
       server.serverPost("burrow.send",this.server,parameters,function(error,data){
@@ -65,7 +73,7 @@
       }
       else
       {
-      parameters = {"priv_key"   : privKey,
+      let parameters = {"priv_key"   : privKey,
       "to_address" : toAddress,
       "amount"     : amount }
       server.serverPost("burrow.sendAndHold",this.server,parameters,function(error,data){
@@ -87,7 +95,7 @@
       }
       else
       {
-      parameters= {    "priv_key" : privKey,
+     let parameters= {    "priv_key" : privKey,
       "address"  : address,
       "data"     : data,
       "gas_limit": gasLimit,
@@ -112,7 +120,7 @@
       else
       {
 
-      parameters={"priv_key" : privKey,
+      let parameters={"priv_key" : privKey,
       "address"  : address,
       "data"     : data,
       "gas_limit": gasLimit,
@@ -138,7 +146,7 @@
       }
       else
       {
-      parameters={"priv_key" : privKey,
+     let parameters={"priv_key" : privKey,
       "name"     : name,
       "data"     : data,
       "amount"   : amount,
@@ -151,6 +159,92 @@
       }
 
       };
+
+
+      /**
+      * Broadcasting a Transaction will send it to the blockchain client. The transaction needs
+      * to be signed in order for this to work.
+      *
+      * @param {Object} tx - The transaction.
+      * @param {module:rpc/rpc~methodCallback} callback - The callback function.
+      */
+      broadcastTx (tx, cb) {
+      if(!this.server ||!tx)
+      {
+      errorMsg ="Method:broadcastTx, error: Invalid arguments"
+      return errorMsg
+      }
+      else
+      {
+     let parameters = tx;
+      server.serverPost("burrow.broadcastTx",this.server,parameters,function(error,data){
+      if(error)  cb(error);
+      cb(null, data);
+
+      });
+      }
+
+      }
+
+      /**
+      * Call the account at the given address. If there is no code in the account, this will do nothing.
+      * Call does not cost anything, and can not affect the state of the database. It is only used to
+      * <tt>get</tt> data.
+      *
+      * @param {string} [fromAddress] - The address from which the call was made. The address is settable because this is a read-only operation.
+      * @param {string} toAddress - The address to the target (contract) account.
+      * @param {string} data - The input data.
+      * @param {module:rpc/rpc~methodCallback} callback - The callback function.
+      */
+
+      call(fromAddress, toAddress, data, cb) {
+      if(!this.server ||!fromAddress ||toAddress ||data)
+      {
+      errorMsg ="Method:call, error: Invalid arguments"
+      return errorMsg
+      }
+      else{
+     let parameters={    "from":fromAddress,
+      "address":toAddress,
+      "data":data
+      }
+      server.serverPost("burrow.call",this.server,parameters,function(error,data){
+      if(error)  cb(error);
+      cb(null, data);
+
+      });
+
+      }
+
+
+      }
+
+      /**
+      * Call the code with the given data as input. This function runs the code in a VM
+      * with the given input.
+      *
+      * @param {string} [fromAddress] - The address from which the call was made. The address is settable because this is a read-only operation.
+      * @param {string} code - The code to be executed.
+      * @param {string} data - The input data.
+      * @param {module:rpc/rpc~methodCallback} callback - The callback function.
+      */
+      callCode (fromAddress, code, data, cb) {
+      if(!this.server ||!fromAddress ||code ||data)
+      {
+      errorMsg ="Method:callCode, error: Invalid arguments"
+      return errorMsg
+      }
+     let parameters=    { "from" : fromAddress,
+      "code" : code,
+      "data" : data
+      }
+      server.serverPost("burrow.callCode",this.server,parameters,function(error,data){
+      if(error)  cb(error);
+      cb(null, data);
+
+      });
+      };
+      
 
 
       /**
