067d47a03a09de9ae643f7941780fecfc65eb7e8
nagarajmanjunath <38091008+nagarajmanjunath@users.noreply.github.com>
2018-06-29 18:12:09 +0800

    updated server js

--- src/lib/server.js
+++ src/lib/server.js
@@ -1,70 +1,69 @@
       /**
-     * @file server.js
-     * @fileOverview Factory module for the server class.
-     * @author Nagaraj Manjunath
-     * @module server
-     */
-    
-    let request = require('request');
-    module.exports = class server {
+       * @file server.js
+       * @fileOverview Factory module for the server class.
+       * @author Nagaraj Manjunath
+       * @module server
+       */
 
-    serverPost(method_Name,url,params,cb)
-    {
-    try {
-        
-        var jsonRandomID=generateJSONID();
-        var commands = {
-            jsonrpc: "2.0",
-            id: jsonRandomID,
-            method:method_Name,
-            params:params,
-        };
-        var options = {
-            method: "POST",
-            url: url,
-            headers: {
-                "content-type": "application/json"
-            },
-            body: commands,
-            json: true
-        };
+      let request = require('request');
+      module.exports = class server {
 
-        // Request burrow client
-        this.requestclient(options, function(err, data) {
+          serverPost(method_Name, url, params, cb) {
+              try {
 
-            if(err) return cb(err);
-            return cb(null, data);
-        });
+                  var jsonRandomID = generateJSONID();
+                  var commands = {
+                      jsonrpc: "2.0",
+                      id: jsonRandomID,
+                      method: method_Name,
+                      params: params,
+                  };
+                  var options = {
+                      method: "POST",
+                      url: url,
+                      headers: {
+                          "content-type": "application/json"
+                      },
+                      body: commands,
+                      json: true
+                  };
 
-        } catch (error) {
-        console.log(error);
-    }
-    };
+                  // Request burrow client
+                  this.requestclient(options, function (err, data) {
+
+                      if (err) return cb(err);
+                      return cb(null, data);
+                  });
+
+              } catch (error) {
+                  console.log(error);
+              }
+          };
 
 
-    /** call request method to burrow to get data */
-    requestclient(options, callback){
-    try {
-        request(options, function (error, response, data) {
-            if (error) return callback(error);
-            return callback(null, data);
-                
-        });
-    }catch (error){
-        throw error;
-    }
-            }
-    }
+          /** call request method to burrow to get data */
+          requestclient(options, callback) {
+              try {
+                  request(options, function (error, response, data) {
+                      if (error) return callback(error);
+                      return callback(null, data);
 
-    /** generate unique  Json RPC 2.0 ID */
-    var generateJSONID=function(){
+                  });
+              } catch (error) {
+                  throw error;
+              }
+          }
+      }
 
-            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-            s4() + '-' + s4() + s4() + s4();
-        
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-        }
-    }
+      /** generate unique  Json RPC 2.0 ID */
+      var generateJSONID = function () {
+
+          return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+              s4() + '-' + s4() + s4() + s4();
+
+          function s4() {
+              return Math.floor((1 + Math.random()) * 0x10000)
+                  .toString(16)
+                  .substring(1);
+          }
+      }
